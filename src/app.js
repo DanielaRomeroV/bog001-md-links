2524c3311889bbee8699085445dcac5d2d426e8c
unknown <dromerovarela776@gmail.com>
2020-09-23 12:14:07 -0500

    validar si el archivo es extension md

--- src/app.js
+++ src/app.js
@@ -1,9 +1,40 @@
 const fs = require('fs');
+const path = require('path');
+const direccion = 'carpeta1/PRUEBA.md';
 
-fs.readFile('PRUEBA.md', {encoding: 'utf8'}, function(error, datos) {
-    if(error){
-        console.log(`Error: ${error}`);
-    } else {
-        console.log(`Datos: ${datos}`);
-    }
-})
+//validar si el archivo es extension md
+if (path.extname(direccion) === '.md') {
+    console.log('si es un markdown');
+    fs.readFile(direccion, { encoding: 'utf8' }, function (error, datos) {
+        if (error) {
+            console.log(`Error: ${error}`);
+        } else {
+            console.log(`Datos: ${datos}`);
+        }
+    })
+} else {
+    console.log('no es markdown')
+};
+
+//volver el archivo md en html y extraer lo que hay dentro (href, file, text)
+
+
+
+//si es archivo o carpeta, recursion asincronia y promesas.
+/*const fs = require('fs').promises;
+const path = require('path');
+
+const tree = (fname) => {
+    return fs.stat(fname);
+.then(stats => {
+        if (!stats.isDirectory()) {
+
+        };
+
+    });
+};
+
+
+tree(path.resolve(process.argv[2]));
+.then(console.log)
+.catch(console.error);*/
